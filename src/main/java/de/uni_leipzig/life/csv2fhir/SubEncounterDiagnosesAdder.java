b2a06c19e83f5ff15f7701626da6f7865e58efb5
Alexander Strübing <alexander.struebing@gmail.com>
2021-11-24 10:27:35 +0100

    Post Processing Collects all still Invalid Encounters
    TODO: Remove these invalid resources from bundle

--- src/main/java/de/uni_leipzig/life/csv2fhir/SubEncounterDiagnosesAdder.java
+++ src/main/java/de/uni_leipzig/life/csv2fhir/SubEncounterDiagnosesAdder.java
@@ -2,6 +2,7 @@ package de.uni_leipzig.life.csv2fhir;
 
 import static de.uni_leipzig.life.csv2fhir.BundleFuntions.getResource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hl7.fhir.r4.model.Bundle;
@@ -31,60 +32,64 @@ public class SubEncounterDiagnosesAdder {
      * @param bundle
      */
     public static void convert(Bundle bundle) {
-        List<BundleEntryComponent> bundleEntries = bundle.getEntry();
+        List<BundleEntryComponent> invalidResources = addMissingDiagnosesToEncounters(bundle); //TODO: should be removed from bundle (incl. all references to these resources??)
+    }
 
-        // for every bundle entry
-        nextEntry: for (BundleEntryComponent entry : bundleEntries) {
+    /**
+     * @param bundle
+     * @return
+     */
+    private static List<BundleEntryComponent> addMissingDiagnosesToEncounters(Bundle bundle) {
+        List<BundleEntryComponent> notValidableResources = new ArrayList<>();
+        nextEntry: for (BundleEntryComponent entry : bundle.getEntry()) {
             Resource resource = entry.getResource();
             if (resource instanceof Encounter) {
                 Encounter encounter = (Encounter) resource;
                 List<DiagnosisComponent> diagnoses = encounter.getDiagnosis();
                 //There is already a diagnosis reference in this encounter -> nothing to do
-                if (!diagnoses.isEmpty()) {
-                    continue;
-                }
-                //Get the Encounter of which this Encounter is a part
-                Reference partOfReference = encounter.getPartOf();
-                String superEncounterID = partOfReference.getReference();
-                //no encounter found from which we can copy a diagnosis reference
-                if (superEncounterID == null) {
-                    continue;
-                }
-                Encounter superEncounter = getResource(bundle, Encounter.class, superEncounterID);
-                if (superEncounter == null) {
-                    continue;
-                }
-                diagnoses = superEncounter.getDiagnosis();
-                //Also the parent Encounter has no diagnosis -> nothing to do
                 if (diagnoses.isEmpty()) {
-                    continue;
-                }
-                //a reference to the first diagnose which has a coded diagnose use from the follwing
-                //iterable will be added to the child encuonter
-                Iterable<String> diagnosisUseCodesInAddingOrder = VersorgungsfallConverter.diagnosisRoleKeyMapper.getValuesInAddingOrder();
-                for (String preferedDiagnosisUseCode : diagnosisUseCodesInAddingOrder) {
-                    for (DiagnosisComponent diagnosis : diagnoses) {
-                        CodeableConcept use = diagnosis.getUse();
-                        if (use != null) {
-                            List<Coding> codings = use.getCoding();
-                            for (Coding coding : codings) {
-                                String code = coding.getCode();
-                                if (Objects.equal(preferedDiagnosisUseCode, code)) {
-                                    encounter.addDiagnosis(diagnosis);
-                                    continue nextEntry;
+                    //Get the Encounter of which this Encounter is a part
+                    Reference partOfReference = encounter.getPartOf();
+                    String superEncounterID = partOfReference.getReference();
+                    //encounter found from which we can copy a diagnosis reference
+                    if (superEncounterID != null) {
+                        Encounter superEncounter = getResource(bundle, Encounter.class, superEncounterID);
+                        if (superEncounter != null) {
+                            diagnoses = superEncounter.getDiagnosis();
+                            //Also the parent Encounter has no diagnosis -> nothing to do
+                            if (!diagnoses.isEmpty()) {
+                                //a reference to the first diagnose which has a coded diagnose use from the follwing
+                                //iterable will be added to the child encuonter
+                                Iterable<String> diagnosisUseCodesInAddingOrder = VersorgungsfallConverter.diagnosisRoleKeyMapper.getValuesInAddingOrder();
+                                for (String preferedDiagnosisUseCode : diagnosisUseCodesInAddingOrder) {
+                                    for (DiagnosisComponent diagnosis : diagnoses) {
+                                        CodeableConcept use = diagnosis.getUse();
+                                        if (use != null) {
+                                            List<Coding> codings = use.getCoding();
+                                            for (Coding coding : codings) {
+                                                String code = coding.getCode();
+                                                if (Objects.equal(preferedDiagnosisUseCode, code)) {
+                                                    encounter.addDiagnosis(diagnosis);
+                                                    continue nextEntry;
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
+                                //no diagnosis has an valid use -> simply add the first to the sub encounter
+                                DiagnosisComponent diagnosisComponent = diagnoses.get(0); //must exists because we check empty above
+                                encounter.addDiagnosis(diagnosisComponent);
                             }
                         }
                     }
                 }
-                //no diagnosis has an valid use -> simply add the first to the sub encounter
-                DiagnosisComponent diagnosisComponent = diagnoses.get(0); //must exists because we check empty above
-                encounter.addDiagnosis(diagnosisComponent);
+                diagnoses = encounter.getDiagnosis();
+                if (diagnoses.isEmpty()) { //Encounter still has no diagnosis -> should be removed
+                    notValidableResources.add(entry);
+                }
             }
         }
-    }
-            }
-        }
+        return notValidableResources;
     }
 
 }
