a4c61a008a58551d23084f1819fe9b30604d2a9b
Alexander Strübing <alexander.struebing@gmail.com>
2022-08-10 10:48:32 +0200

    Update Test Data Excel Template
    Adds the new table Konvertierungsoptionen (Converter Options) to the template and remove unused Columns from laboratory data sheet.

--- src/main/java/de/uni_leipzig/life/csv2fhir/converter/ObservationLaboratoryConverter.java
+++ src/main/java/de/uni_leipzig/life/csv2fhir/converter/ObservationLaboratoryConverter.java
@@ -54,7 +54,6 @@ public class ObservationLaboratoryConverter extends Converter {
                 return "Zeitstempel (Abnahme)";
             }
         },
-        //Methode //not used
     }
 
     /**  */
